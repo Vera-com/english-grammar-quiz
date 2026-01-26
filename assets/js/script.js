efa148f5cc80757a4f5cb0245626695ecfe6e4a2
vera-com <withneyugomma@gmail.com>
2026-01-27 00:46:23 +0100

    Make start Quiz button show quiz screen

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Get elements
+const startBtn = document.getElementById("start-btn");
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
+
+// Start quiz
+startBtn.addEventListener("click", () => {
+  startScreen.hidden = true;
+  quizScreen.hidden = false;
+});
